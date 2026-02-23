7be75663867e3a9f062e1661a148f4575e841f55
sai sai <asuasaira@gmail.com>
2026-02-22 22:26:39 -0300

    Fix image paths by adding public/assets and setting Vite base url

--- src/components/visuals/LazyImage.jsx
+++ src/components/visuals/LazyImage.jsx
@@ -4,15 +4,21 @@ import classes from './LazyImage.module.css';
 export default function LazyImage({ src, alt, className = '', ...props }) {
     const [isLoaded, setIsLoaded] = useState(false);
 
+    // Automatically resolve public assets path regardless of Vite base URL
+    const resolvedSrc = src.startsWith('/') && !src.startsWith('http')
+        ? `${import.meta.env.BASE_URL}${src.slice(1)}`
+        : src;
+
     useEffect(() => {
         const img = new Image();
-        img.src = src;
+        img.src = resolvedSrc;
         img.onload = () => setIsLoaded(true);
-    }, [src]);
+    }, [resolvedSrc]);
 
     return (
         <div className={`${classes.imageContainer} ${className} ${!isLoaded ? 'skeleton' : ''}`}>
-            {isLoaded && <img src={src} alt={alt} className={classes.image} {...props} />}
+            {isLoaded && <img src={resolvedSrc} alt={alt} className={classes.image} {...props} />}
+
         </div>
     );
 }
